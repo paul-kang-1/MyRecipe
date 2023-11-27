4d353c44699439066856d2f1b513e3c6fb334f1c
paul-kang-1 <loiter97@gmail.com>
2023-11-27 01:27:23 -0500

    Initial Commit

---
+++ MyRecipe/MyRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipeApp.swift
+//  MyRecipe
+//
+//  Created by 강우상 on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
